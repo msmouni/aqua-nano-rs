9bdd3e7ab84db7ddc5484ed0ca71b76db5004aae
msmouni <marwan-smouni@hotmail.fr>
2023-07-04 23:26:29 +0200

    🚚 Add rust toolchain at workflow's file

--- compile.sh
+++ compile.sh
@@ -1,4 +1,2 @@
-rustup component add rust-src --toolchain nightly-2022-07-10-x86_64-unknown-linux-gnu
-
 export AVR_CPU_FREQUENCY_HZ=16000000
 cargo build -Z build-std=core --target avr-atmega328p.json --release
